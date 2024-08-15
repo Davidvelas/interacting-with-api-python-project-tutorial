ce7444d190becf9a89aefa01fb486b07eb5f2bb6
Davidvelas <162694070+Davidvelas@users.noreply.github.com>
2024-08-15 06:26:16 +0000

    first

--- src/app.py
+++ src/app.py
@@ -1,7 +1,41 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
+from dotenv import load_dotenv
 import pandas as pd
 import seaborn as sns
-from dotenv import load_dotenv
-
-# load the .env file variables
 load_dotenv()
+def get_artist_top_tracks(artist_id):
+    """Gets the top 10 tracks of an artist."""
+    client_id = os.environ.get("CLIENT_ID")
+    client_secret = os.environ.get("CLIENT_SECRET")
+
+    
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    results = sp.artist_top_tracks(artist_id,)  
+    tracks = results['tracks'][:10]
+
+    track_data = [{
+        'name': track['name'],
+        'popularity': track['popularity'],
+        'duration_ms': track['duration_ms']
+    } for track in tracks]
+
+    return track_data
+     
+
+artist_id = '7dGJo4pcD2V6oG8kP0tJRR'  
+df = pd.DataFrame(get_artist_top_tracks(artist_id))
+print(df)
+
+scatter_plot = sns.scatterplot(data = df, x = "popularity", y = "duration_ms")
+fig = scatter_plot.get_figure()
+fig.savefig("scatter_plot.png")
+
+
+
+
